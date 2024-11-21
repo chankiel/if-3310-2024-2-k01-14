54b395d33799e8bce3a823a5feed1b61b4e93899
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-21 09:35:21 +0700

    feat: setup redis with docker

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,34 @@
 import express from "express";
+import Redis from "ioredis";
 
 const app = express();
 const PORT = process.env.PORT || 4001;
+
+const redis = new Redis({
+  host: "localhost",
+  port: 6379,
+})
+
+redis.on("connect", () => {
+  console.log("Connected to Redis!");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+app.get("/health", async (req, res) => {
+  try {
+    const redisStatus = await redis.ping();
+    if (redisStatus === "PONG") {
+      return res.status(200).send("OK");
+    }
+    throw new Error("Redis not responding");
+  } catch (error) {
+    console.error("Health check failed:", error.message);
+    return res.status(500).send("Redis Unhealthy");
+  }
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
