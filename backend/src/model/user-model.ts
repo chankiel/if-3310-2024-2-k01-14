585d8005f481c7072f90f953318932383f83fb61
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-24 23:21:22 +0700

    feat: add userprismaformat

--- backend/src/model/user-model.ts
+++ backend/src/model/user-model.ts
@@ -1,11 +1,25 @@
 import { FeedFormat } from "./feed-model";
 
 export type UserFormat = {
-  username: string;
-  description: string | null;
-  photo_profile: string | null;
+  name: string | null;
+  work_history: string | null;
+  skills: string | null;
+  profile_photo: string | null;
   relevant_posts: FeedFormat[];
 };
+
+export type UserPrismaFormat = {
+  full_name: string | null;
+    profile_photo_path: string | null;
+    work_history: string | null;
+    skills: string | null;
+    feeds: {
+      id: number;
+      created_at: Date;
+      updated_at: Date;
+      content: string;
+    }[];
+}
 
 export type CreateUserRequest = {
   username: string;
