528d8113d83d4f21a6ad70f8cc6972d1bb166619
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-25 15:08:08 +0700

    feat: multer

--- backend/src/route/public-api.ts
+++ backend/src/route/public-api.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
 import { UserController } from "../controller/user-controller";
 import { ConnectionController } from "../controller/connection-controller";
+import multer from "multer";
 
 const publicRouter = express.Router();
+const upload = multer()
 
-publicRouter.post("/register", UserController.store);
-publicRouter.post("/login", UserController.login);
+publicRouter.post("/register", upload.none(), UserController.store);
+publicRouter.post("/login", upload.none(), UserController.login);
 
 publicRouter.get("/users/:query?",UserController.index)
 publicRouter.get("/connections/:user_id(\\d+)",ConnectionController.indexConnection)
