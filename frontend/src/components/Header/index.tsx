174f408466e695ac1d6fed848e1271b574459c80
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-26 14:33:52 +0700

    feat: navbar, shadcn

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,67 +1,86 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UseAuth } from "../../contexts/AuthContext";
+import {
+  ChatIcon,
+  GroupIcon,
+  HomeIcon,
+  LinkedInIcon,
+  NotificationIcon,
+} from "../Icons";
+import { MagnifyingGlassIcon, ClockIcon } from "@heroicons/react/24/solid";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
-interface User {
-  role: string;
-  nama: string;
-}
+const Header: React.FC = () => {
+  const { isAuthenticated, login, logout, username, name } = UseAuth();
 
-interface HeaderProps {
-  user: User | null;
-}
-
-const Header: React.FC<HeaderProps> = ({ user }) => {
-  const [popoverVisible, setPopoverVisible] = useState(false);
-
-  const togglePopover = () => setPopoverVisible((prev) => !prev);
+  const paths = [
+    {
+      path: "/users",
+      name: "Search",
+      icon: <MagnifyingGlassIcon height={24} width={24} />,
+    },
+    {
+      path: "/feed",
+      name: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      path: "/mynetworks/connections",
+      name: "My Connections",
+      icon: <GroupIcon />,
+    },
+    {
+      path: "/mynetworks/pending",
+      name: "Requests",
+      icon: <ClockIcon height={27} width={27} />,
+    },
+    {
+      path: "/chat",
+      name: "Chat",
+      icon: <ChatIcon />,
+    },
+    {
+      path: "/notification",
+      name: "Notification",
+      icon: <NotificationIcon />,
+    },
+  ];
 
   return (
     <header className="bg-white sticky top-0 z-20 h-16 py-1 px-4 mb-2 text-gray-800">
       <div className="container flex justify-between items-center h-full mx-auto">
-        <a className="w-2/3 h-full" href="/">
-          <img
-            src="/public/images/linkedin.svg"
-            width="150"
-            height="100"
-            alt="logo"
-            className="h-auto max-h-full border-transparent"
-          />
+        <a className="md:w-full h-full" href="/">
+          <LinkedInIcon color="#0a66c2" size={50} />
         </a>
-        <nav>
-          <ul className="flex items-center gap-2">
-            {user ? (
+        <nav className="w-2/3">
+          <ul className="flex justify-around gap-2 text-sm text-gray-500">
+            {!isAuthenticated ? (
               <>
-                <li className="flex flex-col items-center relative">
-                  <a href="/">
-                    <span className="material-symbols-outlined text-3xl">
-                      home
-                    </span>
-                    <p>Home</p>
-                  </a>
-                </li>
-                {user.role === "jobseeker" && (
-                  <li className="flex flex-col items-center relative">
-                    <a href="/applications">
-                      <span className="material-symbols-outlined text-3xl">
-                        history
-                      </span>
-                      <p>History</p>
-                    </a>
+                {paths.map((path) => (
+                  <li className="relative hover:text-black flex items-center justify-center md:min-w-14">
+                    <Link to={path.path} className="flex flex-col items-center">
+                      {path.icon}
+                      <p className="hidden md:block">{path.name}</p>
+                    </Link>
                   </li>
-                )}
-                <li className="flex flex-col items-center relative" onClick={togglePopover}>
-                  <img
-                    src="/public/images/perry-casino.webp"
-                    alt="logo"
-                    className="h-7 w-7 rounded-full"
-                  />
-                  <p className="flex items-center">
-                    Me{" "}
-                    <span className="material-symbols-outlined">
-                      arrow_drop_down
-                    </span>
-                  </p>
-                  {popoverVisible && (
-                    <div className="absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md w-72 p-2 z-10">
+                ))}
+                <li className="flex flex-col items-center relative">
+                  <Popover>
+                    <PopoverTrigger>
+                      <Avatar className="h-7 w-7">
+                        <AvatarImage src="/images/perry-casino.webp" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+
+                      <p className="md:flex items-center hidden">
+                        Me{" "}
+                        <span className="material-symbols-outlined">
+                          arrow_drop_down
+                        </span>
+                      </p>
+                    </PopoverTrigger>
+                    <PopoverContent align="end" sideOffset={15}>
                       <div className="flex gap-2 mb-4">
                         <img
                           src="/public/images/perry-casino.webp"
@@ -69,35 +88,48 @@ const Header: React.FC<HeaderProps> = ({ user }) => {
                           className="h-20 w-20 object-cover rounded-full"
                         />
                         <div className="text-left">
-                          <h1 className="font-medium text-sm">{user.nama}</h1>
-                          <h2 className="font-bold text-sm capitalize">
-                            {user.role}
-                          </h2>
+                          <h1 className="font-medium text-lg">
+                            Ignatius Jhon Hezkiel Chan
+                          </h1>
                         </div>
                       </div>
-                      {user.role === "company" && (
-                        <a href="/profile/company" className="block mb-3 text-blue-600 border border-blue-600 rounded-lg p-2">
-                          View Profile
-                        </a>
-                      )}
-                      <form action="/logout" method="POST" className="text-gray-600">
-                        <button type="submit" className="w-full text-left underline">
+                      <Link
+                        to={"/profile/company"}
+                        className="block mb-3 text-linkin-blue font-semibold border border-linkin-blue rounded-xl px-2 text-center transition-all duration-100 ease-in hover:ring-linkin-dark-blue hover:ring-2 hover:bg-linkin-hoverblue"
+                      >
+                        View Profile
+                      </Link>
+                      <form
+                        action="/logout"
+                        method="POST"
+                        className="text-gray-600"
+                      >
+                        <button
+                          type="submit"
+                          className="w-full text-left hover:underline"
+                        >
                           Logout
                         </button>
                       </form>
-                    </div>
-                  )}
+                    </PopoverContent>
+                  </Popover>
                 </li>
               </>
             ) : (
               <>
                 <li>
-                  <a href="/login" className="py-2 px-4 rounded-full border-2 border-blue-600 text-blue-600 hover:bg-blue-100">
+                  <a
+                    href="/login"
+                    className="py-2 px-4 rounded-full border-2 border-blue-600 text-blue-600 hover:bg-blue-100"
+                  >
                     Login
                   </a>
                 </li>
                 <li>
-                  <a href="/register" className="py-2 px-4 rounded-full bg-blue-600 text-white hover:bg-blue-700">
+                  <a
+                    href="/register"
+                    className="py-2 px-4 rounded-full bg-blue-600 text-white hover:bg-blue-700"
+                  >
                     Register
                   </a>
                 </li>
