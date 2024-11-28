319e3cab3753d72fe7261560365faf4c34becf3f
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-29 03:27:39 +0700

    feat: connect api

--- frontend/src/api/connection-api.ts
+++ frontend/src/api/connection-api.ts
@@ -1,45 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios"
+import axios from "axios";
 
 import { API_URL } from "../constant";
 import { ConnectionFormat, ConnectionResponse } from "../types";
 
-class ConnectionApi{
-    private static axios = axios.create({
-        baseURL: API_URL,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        withCredentials: true,
-    });
+class ConnectionApi {
+  private static axios = axios.create({
+    baseURL: API_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
 
-    static async getConnections(user_id: number): Promise<ConnectionFormat[]>{
-        try{
-            const response = await this.axios.get<ConnectionResponse>(`/connections/${user_id}`)
-            return response.data.body
-        }catch(error){
-            throw (error as any)?.response?.data;
-        }
+  static async getConnections(user_id: number): Promise<ConnectionFormat[]> {
+    try {
+      const response = await this.axios.get<ConnectionResponse>(
+        `/connections/${user_id}`
+      );
+      return response.data.body;
+    } catch (error) {
+      throw (error as any)?.response?.data;
     }
+  }
 
-    static async getPendingRequests(user_id: number ): Promise<ConnectionFormat[]>{
-        try{
-            const response = await this.axios.get<ConnectionResponse>(`/connection-requests/${user_id}/pending`)
+  static async getPendingRequests(
+    user_id: number
+  ): Promise<ConnectionFormat[]> {
+    const response = await this.axios.get<ConnectionResponse>(
+      `/connection-requests/${user_id}/pending`
+    );
 
-            return response.data.body
-        }catch(error){
-            throw (error as any)?.response?.data;
-        }
+    return response.data.body;
+  }
+
+  static async deleteConnection(from_id: number, to_id: number): Promise<void> {
+    try {
+      await this.axios.delete<void>(`/connections/${from_id}/${to_id}`);
+    } catch (error) {
+      throw (error as any)?.response?.data;
     }
-
-    static async deleteConnection(from_id: number, to_id: number): Promise<void>{
-        try{
-            await this.axios.delete<void>(`/connections/${from_id}/${to_id}`)
-        }catch(error){
-            throw (error as any)?.response?.data;
-        }
-    }
-
+  }
 }
 
-export default ConnectionApi
+export default ConnectionApi;
--- frontend/src/pages/Request/index.tsx
+++ frontend/src/pages/Request/index.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import { Sidebar } from "../../components";
 import { RequestCard } from "../../components/Connection";
+import { ConnectionFormat } from "../../types";
+import ConnectionApi from "../../api/connection-api";
+import { UseAuth } from "../../contexts/AuthContext";
 
 const Requests = () => {
+  const {currentId} = UseAuth()
+  const [requests, setRequests] = useState<ConnectionFormat[]>([])
+
+  useEffect(()=>{
+    const fetchRequests = async ()=>{
+      if(currentId===0) return
+      try{
+        const body = await ConnectionApi.getPendingRequests(currentId)
+        setRequests(body)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    fetchRequests()
+  },[currentId])
+
   return (
     <>
       <Sidebar />
       <section className="py-5">
         <h1 className="px-5 text-xl font-semibold mb-3">Manage Requests</h1>
-        <RequestCard/>
-        <RequestCard/>
-        <RequestCard/>
-        <RequestCard/>
+        {requests && requests.length > 0 ? requests.map((req,index)=>(
+          <RequestCard key={index}/>
+        )) : 
+        <div className="w-5/6 mx-auto flex flex-col items-center mt-10">
+          <img src="/images/no-request.png" alt="no-request" className="w-1/2" />
+          <h1 className="text-[30px] mt-3">No new requests</h1>
+        </div>
+        }
       </section>
     </>
   );
