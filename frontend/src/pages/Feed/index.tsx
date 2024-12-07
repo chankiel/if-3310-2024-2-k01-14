b0cd875c0a3b3db63483146eb07d81050a705c8f
HanifIHM <imamhanif2003@gmail.com>
2024-12-07 22:17:42 +0700

    feat : ubah tampilan feeds

--- frontend/src/pages/Feed/index.tsx
+++ frontend/src/pages/Feed/index.tsx
@@ -118,12 +118,7 @@ export default function Feed() {
         };
         
         fecthUlang();
-      }, [update]);
-
-    // useEffect(() => {
-    //     setEditContent(editContent); // Update postContent whenever content changes
-    // }, [editContent]);
-    
+      }, [update]);    
 
     // Menggunakan efek untuk mendeteksi scroll dan memuat halaman berikutnya
     useEffect(() => {
@@ -203,117 +198,114 @@ export default function Feed() {
 
     return (
         <>
-            <main className="flex flex-col md:flex-row p-4 justify-center gap-x-6">
-                <Sidebar />
-                <div className="min-w-[600px] max-w-[600px] flex-1">
-                    <div className="flex items-center w-full mb-4 border rounded-lg px-8 py-6 bg-white">
-                        <div className="w-1/7">
-                            <img
-                                src="/perry-casino.webp"
-                                alt="Profile"
-                                className="w-14 h-14 rounded-full"
-                            />
-                        </div>
-                        <button
-                            className="flex-1 ml-4 p-4 border border-gray-400 rounded-3xl text-left focus:outline-none hover:bg-gray-100"
-                            onClick={handleNewPostClick}
-                        >
-                            Start a post, try writing with AI
-                        </button>
+            <Sidebar />
+            <section className="flex-1 bg-transparent border-0">
+                <div className="flex items-center w-full mb-4 border rounded-lg px-8 py-6 bg-white">
+                    <div className="w-1/7">
+                        <img
+                            src="/perry-casino.webp"
+                            alt="Profile"
+                            className="w-14 h-14 rounded-full"
+                        />
                     </div>
-                    <h1 className="px-5 text-xl font-semibold mb-3">
-                        {hasFeed
-                            ? ``
-                            : "You don't have a Feed yet."}
-                    </h1>
-                    {isLoading ? (
-                        <p>Loading feeds...</p>
-                    ) : isError ? (
-                        <p>Failed to load feeds.</p>
-                    ) : hasFeed ? (
-                        <ul>
-                            {feeds.map((feed, index) => (
-                                <li
-                                key={index}
-                                className="relative flex flex-row py-4 px-4 border-b bg-white mb-4 border rounded-lg"
-                            >
-                                <div>
-                                    <div className="flex items-center w-full pb-2">
-                                        <div className="w-1/7 pl-4">
-                                            <img
-                                                src={
-                                                    feed.user.profile_photo_path
-                                                        ? feed.user.profile_photo_path
-                                                        : "/perry-casino.webp"
-                                                }
-                                                alt="Profile"
-                                                className="w-14 h-14 rounded-full"
-                                            />
-                                        </div>
-                                        <div className="flex-1 w-6/7 px-4">
-                                            <div className="py-2">
-                                                <div className="font-semibold text-base">
-                                                    {feed.user.username}
-                                                </div>
-                                                <div className="font-normal text-xs text-gray-500">
-                                                    posted {moment(feed.created_at).fromNow()}
-                                                </div>
-                                                <div className="font-normal text-xs text-gray-500">
-                                                    updated {moment(feed.updated_at).fromNow()}
-                                                </div>
+                    <button
+                        className="flex-1 ml-4 p-4 border border-gray-400 rounded-3xl text-left focus:outline-none hover:bg-gray-100"
+                        onClick={handleNewPostClick}
+                    >
+                        Start a post, try writing with AI
+                    </button>
+                </div>
+                <h1 className="px-5 text-xl font-semibold mb-3">
+                    {hasFeed
+                        ? ``
+                        : "You don't have a Feed yet."}
+                </h1>
+                {isLoading ? (
+                    <p>Loading feeds...</p>
+                ) : isError ? (
+                    <p>Failed to load feeds.</p>
+                ) : hasFeed ? (
+                    <ul>
+                        {feeds.map((feed, index) => (
+                            <li
+                            key={index}
+                            className="relative flex flex-row py-4 px-4 border-b bg-white mb-4 border rounded-lg"
+                        >
+                            <div>
+                                <div className="flex items-center w-full pb-2">
+                                    <div className="w-1/7 pl-4">
+                                        <img
+                                            src={
+                                                feed.user.profile_photo_path
+                                                    ? feed.user.profile_photo_path
+                                                    : "/perry-casino.webp"
+                                            }
+                                            alt="Profile"
+                                            className="w-14 h-14 rounded-full"
+                                        />
+                                    </div>
+                                    <div className="flex-1 w-6/7 px-4">
+                                        <div className="py-2">
+                                            <div className="font-semibold text-base">
+                                                {feed.user.username}
+                                            </div>
+                                            <div className="font-normal text-xs text-gray-500">
+                                                posted {moment(feed.created_at).fromNow()}
+                                            </div>
+                                            <div className="font-normal text-xs text-gray-500">
+                                                updated {moment(feed.updated_at).fromNow()}
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="text-xl ml-4">{feed.content}</div>
                                 </div>
-                            
-                                {/* Tombol di kanan atas */}
-                                <div
-                                    className={`absolute top-6 right-2 flex gap-2 ${
-                                        currentId === feed.user.id ? "" : "hidden"
-                                    }`}
+                                <div className="text-xl ml-4">{feed.content}</div>
+                            </div>
+                        
+                            <div
+                                className={`absolute top-6 right-4 flex gap-2 ${
+                                    currentId === feed.user.id ? "" : "hidden"
+                                }`}
+                            >
+                                <button
+                                    onClick={() => handleEdit(feed.id, feed.content)}
+                                    className="p-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600"
+                                    aria-label="Edit"
                                 >
-                                    <button
-                                        onClick={() => handleEdit(feed.id, feed.content)}
-                                        className="p-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600"
-                                        aria-label="Edit"
-                                    >
-                                        <PencilIcon height={20} width={20} />
-                                    </button>
-                                    <button
-                                        onClick={() => handleDelete(feed.id)}
-                                        className="p-2 text-sm text-white bg-red-500 rounded hover:bg-red-600"
-                                        aria-label="Delete"
-                                    >
-                                        <TrashIcon height={20} width={20} />
-                                    </button>
-                                </div>
-                            </li>
-                            
-                            ))}
-                        </ul>
-                    ) : (
-                        <div className="flex flex-col items-center">
-                            <img
-                                src="/images/no-request.png"
-                                alt="no-connection"
-                                className="w-1/2 min-w-52"
-                            />
-                            <h2 className="px-5 text-lg mt-5">
-                                No Feed
-                            </h2>
-                        </div>
-                    )}
-                    {hasNextPage && (isFetchingNextPage && <div>Loading more...</div>)}
-                    <div ref={loadMoreRef} />
-                </div>
-                <div className="max-w-sm">
-                    <RecommendationSection recommendations={recommendations} />
-                </div>
-            </main>
+                                    <PencilIcon height={20} width={20} />
+                                </button>
+                                <button
+                                    onClick={() => handleDelete(feed.id)}
+                                    className="p-2 text-sm text-white bg-red-500 rounded hover:bg-red-600"
+                                    aria-label="Delete"
+                                >
+                                    <TrashIcon height={20} width={20} />
+                                </button>
+                            </div>
+                        </li>
+                        
+                        ))}
+                    </ul>
+                ) : (
+                    <section className="flex flex-col items-center">
+                        <img
+                            src="/images/no-request.png"
+                            alt="no-connection"
+                            className="w-1/2 min-w-52"
+                        />
+                        <h2 className="px-5 text-lg mt-5">
+                            There is no feed Here
+                        </h2>
+                    </section>
+                )}
+                {hasNextPage && (isFetchingNextPage && <div>Loading more...</div>)}
+                <div ref={loadMoreRef} />
+            </section>
+            <div className="max-w-sm">
+                <RecommendationSection recommendations={recommendations} />
+            </div>
 
-            <CreatePostModal isOpen={isModalOpen} onClose={handleCloseModal} onAddFeed={handleNewPost}/>
-            <EditPostModal isOpen={isModalEdit} onClose={handleCloseModalEdit} feed_id={feedIdEdit} user_id={currentId} username={username} content={editContent} onUpdate={handleUpdateFeed} />
-        </>
+        <CreatePostModal isOpen={isModalOpen} onClose={handleCloseModal} onAddFeed={handleNewPost}/>
+        <EditPostModal isOpen={isModalEdit} onClose={handleCloseModalEdit} feed_id={feedIdEdit} user_id={currentId} username={username} content={editContent} onUpdate={handleUpdateFeed} />
+    </>
     );
 }
