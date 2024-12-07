810697b251cfda54874fee4cba204c8ebd436631
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-12-07 15:51:22 +0700

    fix: remove id from prisma query

--- backend/src/controller/auth-controller.ts
+++ backend/src/controller/auth-controller.ts
@@ -36,7 +36,6 @@ export class AuthController{
                 id: req.userId
             },
             select:{
-                id: true,
                 ...prismaUserFormat
             }
         })
